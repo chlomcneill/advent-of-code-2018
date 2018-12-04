44c24df0319f6c694b4cbc08c73f699da5256f4e
chlomcneill <chloe.mcneill@outlook.com>
2018-12-04 17:11:33 +0000

    Adding Day 3 files, incomplete

---
+++ Day 3/Day3Part1.py
@@ -0,0 +1,25 @@
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open("/Users/mcneillc/Documents/advent-of-code-2018/Day 3/Day3input.txt","r")
+claims = f.readlines()
+f.close()
+
+# size = 4
+# grid = []
+
+# for y in range(size):
+#    grid = grid + [[]]
+#    for x in range(size):
+#       grid[y] = grid[y] + [0]
+
+# print(grid)
+
+xvalues = np.array(range(10))
+yvalues = np.array(range(10))
+
+xx, yy = np.meshgrid(xvalues, yvalues)
+
+plt.plot(xx, yy, marker = '.', color = 'k', linestyle = 'none')
+# plt.show()
