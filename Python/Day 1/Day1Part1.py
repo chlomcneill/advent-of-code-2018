cb34966e0f7e02a8ced13a44bf695304636b0242
chlomcneill <chloe.mcneill@outlook.com>
2018-12-19 14:40:03 +0000

    Updates

--- Python/Day 1/Day1Part1.py
+++ Python/Day 1/Day1Part1.py
@@ -2,13 +2,5 @@ f = open("/Users/mcneillc/Documents/advent-of-code-2018/Python/Day 1/Day1input.t
 frequency_changes = f.readlines()
 f.close()
 
-# current_frequency = 0
-# for change in frequency_changes:
-#     current_frequency += int(change)
-# print(current_frequency)
-
 frequency_changes = [int(i) for i in frequency_changes]
-
-total = sum(frequency_changes)
-
-print(total)
+print(sum(frequency_changes))
